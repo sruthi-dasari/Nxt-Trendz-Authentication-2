8648f176ec011b4867b7b320368c9ac81b0480a3
sruthi <sruthi@example.com>
2023-01-26 11:48:07 +0000

    jwtToken not getting deleted when logout is clicked

---
+++ src/components/Products/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+import Header from '../Header'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="products-image"
+      />
+    </div>
+  </>
+)
+
+export default Products
